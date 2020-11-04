79d68d61374694ad30263cf84a16ee701a78444a
dasol21 <dasol_21@naver.com>
2020-11-04 20:22:00 +0900

    add comment

--- app.py
+++ app.py
@@ -42,5 +42,6 @@ def hate_restaurant():
     return jsonify({'result': 'success', 'msg': '싫어요'})
 
 # 수정기록 남기는 테스트입니다.
+# 슬랙 연결했지롱
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
